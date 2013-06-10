30ca41b7018901a4f1a57a37f61e9fca5c38cad0
Vinodhkumar Srinivasan <kumarsvinodh@gmail.com>
2013-06-10 17:33:55 -0400

    s2c sample

---
+++ assets/www/app/view/Main.js
@@ -0,0 +1,48 @@
+Ext.define('s2c.view.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'main',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [
+            {
+                title: 's2c',
+                iconCls: 'home',
+
+                styleHtmlContent: true,
+                scrollable: true,
+
+                items: {
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'S2C'
+                },
+
+                html: [
+                    "Welcome to Search2Close."
+                ].join("")
+            },
+            {
+                title: 'Users',
+                iconCls: 'star',
+
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'User Details'
+                    },
+                    {
+                        xtype: 'video',
+                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
+                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
+                    }
+                ]
+            }
+        ]
+    }
+});
---
+++ gen/com/search2close/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.search2close;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
